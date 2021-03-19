155f7df57a1108548abb361cec480427cba0fc06
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2021-03-18 20:41:17 -0700

    passed tests for add & find values

--- lib/tree.rb
+++ lib/tree.rb
@@ -16,16 +16,50 @@ class Tree
     @root = nil
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  # Time Complexity: O(log n) for balanced tree/ O(n) for unbalanced tree,
+  #                  because the worst case scenario is to run thru the whole height of the binary tree to add a leave,
+  #                  and it only takes half amount of the elements on the binary tree for searching.
+  # Space Complexity: O(1), because the variables used is a constant.
   def add(key, value)
-    raise NotImplementedError
+    return @root = TreeNode.new(key, value) if @root.nil?
+
+    pointer = @root
+    previous = nil
+    while pointer
+      previous = pointer
+      if pointer.key <= key
+        pointer = pointer.right
+      else
+        pointer = pointer.left
+      end
+    end 
+
+    if previous.key <= key
+      previous.right = TreeNode.new(key, value)
+    else
+      previous.left = TreeNode.new(key, value)
+    end
+    return @root
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  # Time Complexity: O(log n) for balanced tree/ O(n) for unbalanced tree,
+  #                  because the worst case scenario is to run thru the whole height of the binary tree to find a leave,
+  #                  and it only takes half amount of the elements on the binary tree for searching.
+  # Space Complexity: O(1), because the variables used is a constant. 
   def find(key)
-    raise NotImplementedError
+    find = nil
+    pointer = @root
+    while pointer
+      if pointer.key == key
+        find = pointer.value
+        break
+      elsif pointer.key <= key
+        pointer = pointer.right
+      else
+        pointer = pointer.left
+      end
+    end
+    return find
   end
 
   # Time Complexity: 
