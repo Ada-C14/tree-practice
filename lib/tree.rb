3537ded2a23eaae3f7374e40f9e8d22ea60bb387
Tram Bui <pbui17@cmc.edu>
2021-03-20 11:58:06 -0500

    completed the add and find methods

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,12 +2,13 @@ class TreeNode
   attr_reader :key, :value
   attr_accessor :left, :right
 
-   def initialize(key, val)
-    @key = key
-    @value = val
-    @left = nil
-    @right = nil
-   end
+  def initialize(key, val)
+   @key = key
+   @value = val
+   @left = nil
+   @right = nil
+  end
+  
 end
 
 class Tree
@@ -16,16 +17,43 @@ class Tree
     @root = nil
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
-  def add(key, value)
-    raise NotImplementedError
+  def add_helper(current, key, value)
+    
+    if current.nil?
+      current = TreeNode.new(key, value)
+    elsif key <= current.key 
+     current.left = add_helper(current.left, key, value)
+    else
+     current.right = add_helper(current.right, key, value)
+    end
+ 
+    return current
+
   end
 
-  # Time Complexity: 
+  # Time Complexity: O(log n)
+  # Space Complexity: 
+  def add(key, value)
+    @root = add_helper(@root, key, value)
+  end
+
+  def find_helper(current, key)
+    return nil if current.nil?
+
+    if current.key == key
+      return current.value
+    elsif key < current.key
+      find_helper(current.left, key)
+    else # key > root.key
+      find_helper(current.right, key)
+    end
+
+  end
+
+  # Time Complexity: O(log n)
   # Space Complexity: 
   def find(key)
-    raise NotImplementedError
+    find_helper(@root, key)
   end
 
   # Time Complexity: 
