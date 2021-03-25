5c13189b6c754df6bc120b64c413839401e8e27a
Anna Kim <anna.kim93@gmail.com>
2021-03-25 16:47:03 -0700

    add node to tree

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,12 +2,12 @@ class TreeNode
   attr_reader :key, :value
   attr_accessor :left, :right
 
-   def initialize(key, val)
+  def initialize(key, val)
     @key = key
-    @value = val
-    @left = nil
-    @right = nil
-   end
+   @value = val
+   @left = nil
+   @right = nil
+  end
 end
 
 class Tree
@@ -16,10 +16,28 @@ class Tree
     @root = nil
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  # Time Complexity: O(log n)
+  # >> Assuming balanced BST, O(log n) time complexity (half the num nodes to check each time)
+  # >> If not balanced, could by O(n)
+  # Space Complexity: O(1)
+  # >> keep track of current tree node
   def add(key, value)
-    raise NotImplementedError
+    @root = TreeNode.new(key, value) if @root.nil?
+    
+    current = @root
+    while current.right && current.left
+      current = if key > current.key
+                  current.right
+                else
+                  current.left
+                end
+    end
+    
+    if key > current.key
+      current.right = TreeNode.new(key, value)
+    else
+      current.left = TreeNode.new(key, value)
+    end
   end
 
   # Time Complexity: 
