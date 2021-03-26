8fcebd7d79d97b7a330c03ba486179131de231ae
Sophie <sophie.messing@realself.com>
2021-03-25 17:43:15 -0700

    add, find, inorder, preorder, postorder with recursive helper methods

--- lib/tree.rb
+++ lib/tree.rb
@@ -16,35 +16,91 @@ class Tree
     @root = nil
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  # Time Complexity: O(1)
+  # Space Complexity: O(n)
   def add(key, value)
-    raise NotImplementedError
+    new_node = TreeNode.new(value)
+    if @root.nil?
+      @root = new_node
+    else 
+      add_helper(@root, new_node)
+    end 
+  end
+
+  def add_helper(current, new_node)
+    if new_node.value <= current.value
+      current.left = add_helper(current.left, new_node)
+    elsif new_node.value => current.value
+      current.right = add_helper(curent.right, new_node)
+    end 
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def find(key)
-    raise NotImplementedError
+    return nil if @root.nil?
+    return find_helper(@root, key)
+  end
+
+  def find_helper(curent_node, key)
+    return nil if current_node.nil?
+  
+    if key < current_node.key
+      return find_helper(current_node.left, key)
+    elsif key > current_node.key
+      return find_helper(current_node.right, key)
+    elsif key == current_node.key
+      return current_node.value
+    end
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def inorder
-    raise NotImplementedError
+    return [] if @root.nil?
+    return inorder_helper(@root, [])
   end
+
+  def inorder_helper(current, values)
+    return values if current.nil?
+
+    inorder_helper(current.left, values)
+    values.push(current.value)
+    inorder_helper(current.right, value)
+    return values
+  end 
 
   # Time Complexity: 
   # Space Complexity: 
   def preorder
-    raise NotImplementedError
+    return [] if @root.nil?
+    return preorder_helper(@root, [])
   end
+  
+  def preorder_helper(current, values)
+    return values if current.nil?
+
+    values.push(current.value)
+    preorder_helper(current.left, values)
+    preorder_helper(current.right, value)
+    return values
+  end 
 
   # Time Complexity: 
   # Space Complexity: 
   def postorder
-    raise NotImplementedError
+    return [] if @root.nil?
+    return inorder_helper(@root, [])
   end
+
+  def postorder_helper(current, values)
+    return values if current.nil?
+
+    postorder_helper(current.left, values)
+    postorder_helper(current.right, value)
+    values.push(current.value)
+    return values
+  end 
 
   # Time Complexity: 
   # Space Complexity: 
