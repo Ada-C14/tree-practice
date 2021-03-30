4a4bc1321b0b5249f512f45cc1e766e5da4e469a
gessica <gessicamohr18@gmail.com>
2021-03-29 22:08:45 -0700

    completed the first 5 methods, still need to add time and space complexity

--- lib/tree.rb
+++ lib/tree.rb
@@ -16,47 +16,123 @@ class Tree
     @root = nil
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
-  def add(key, value)
-    raise NotImplementedError
+  def add_helper(current, new_node)
+  return new_node if current.nil?
+
+  if new_node.key <= current.key 
+    current.left = add_helper(current.left, new_node)
+  else
+    current.right = add_helper(current.right, new_node)
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  return current
+  end
+
+  # Time Complexity: O(logn(n))
+  # Space Complexity: O(n)
+  def add(key, value = nil)
+    # raise NotImplementedError
+    new_node = TreeNode.new(key, value)
+    if @root.nil?
+      @root = new_node
+    else
+      add_helper(@root, new_node)
+    end
+  end
+
+  def find_helper(current, key)
+    return nil if current.nil?
+
+    if current.key == key
+      return current.value
+    elsif key < current.key
+      find_helper(current.left, key )
+      
+    else 
+      find_helper(current.right, key )
+    end
+
+  end
+
+  # Time Complexity: O(log(n))
+  # Space Complexity: O(n)
   def find(key)
-    raise NotImplementedError
+    # raise NotImplementedError
+    if @root.nil?
+      return nil
+    else
+      return find_helper(@root, key)
+    end
+  end
+
+  def inorder_helper(current, values)
+    if current.nil?
+      return values
+    end
+
+    inorder_helper(current.left, values)
+    values.push({key: current.key, value: current.value})
+    inorder_helper(current.right, values)
+    return values
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def inorder
-    raise NotImplementedError
+    # raise NotImplementedError
+    values = []
+    return inorder_helper(@root, values)
+  end
+
+  def preorder_helper(current, values)
+    if current.nil?
+      return values
+    end
+
+    values.push({key: current.key, value: current.value})
+    preorder_helper(current.left, values)
+    preorder_helper(current.right, values)
+    return values
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def preorder
-    raise NotImplementedError
+    # raise NotImplementedError
+    values = []
+    return preorder_helper(@root, values)
+  end
+
+  def postorder_helper(current, values)
+    if current.nil?
+      return values
+    end
+
+    postorder_helper(current.left, values)
+    postorder_helper(current.right, values)
+    values.push({key: current.key, value: current.value})
+    return values
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def postorder
-    raise NotImplementedError
+    # raise NotImplementedError
+    values = []
+    return postorder_helper(@root, values)
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def height
-    raise NotImplementedError
+    # raise NotImplementedError
   end
 
   # Optional Method
   # Time Complexity: 
   # Space Complexity: 
   def bfs
-    raise NotImplementedError
+    # raise NotImplementedError
   end
 
   # Useful for printing
