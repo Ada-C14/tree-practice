a7d55573e73abe9107d53a297888b53e040a0f1c
beauttie <15842537+beauttie@users.noreply.github.com>
2021-03-30 23:08:59 -0700

    Implement all methods except bfs to pass tests

--- lib/tree.rb
+++ lib/tree.rb
@@ -16,47 +16,118 @@ class Tree
     @root = nil
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
-  def add(key, value)
-    raise NotImplementedError
+  # extra info/state to carry over => helper function
+
+  # Time Complexity: O(log n)
+  # Space Complexity: O(log n)
+  def add(key, value = nil)
+    if @root.nil?
+      @root = TreeNode.new(key, value)
+      return
+    end
+
+    add_helper(@root, key, value)
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  def add_helper(node, key, value)
+    if key <= node.key
+      if node.left.nil?
+        node.left = TreeNode.new(key, value)
+        return
+      else
+        add_helper(node.left, key, value)
+      end
+    else
+      if node.right.nil?
+        node.right = TreeNode.new(key, value)
+        return
+      else
+        add_helper(node.right, key, value)
+      end
+    end
+  end
+
+  # Time Complexity: O(log n)
+  # Space Complexity: O(log n)
   def find(key)
-    raise NotImplementedError
+    find_helper(@root, key)
+  end
+
+  def find_helper(node, key)
+    if node.nil?
+      return nil
+    elsif key == node.key
+      return node.value
+    elsif key < node.key
+      find_helper(node.left, key)
+    elsif key > node.key
+      find_helper(node.right, key)
+    end
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def inorder
-    raise NotImplementedError
+    return inorder_helper(@root, [])
+  end
+
+  def inorder_helper(node, traversal)
+    return traversal if node.nil?
+
+    inorder_helper(node.left, traversal)
+    traversal << {key: node.key, value: node.value}
+    inorder_helper(node.right, traversal)
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def preorder
-    raise NotImplementedError
+    return preorder_helper(@root, [])
+  end
+
+  def preorder_helper(node, traversal)
+    return traversal if node.nil?
+
+    traversal << {key: node.key, value: node.value}
+    preorder_helper(node.left, traversal)
+    preorder_helper(node.right, traversal)
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def postorder
-    raise NotImplementedError
+    return postorder_helper(@root, [])
+  end
+
+  def postorder_helper(node, traversal)
+    return traversal if node.nil?
+
+    postorder_helper(node.left, traversal)
+    postorder_helper(node.right, traversal)
+    traversal << {key: node.key, value: node.value}
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def height
-    raise NotImplementedError
+    return height_helper(@root)
+  end
+
+  def height_helper(node)
+    return 0 if node.nil?
+
+    return 1 + [height_helper(node.left), height_helper(node.right)].max
   end
 
   # Optional Method
   # Time Complexity: 
   # Space Complexity: 
   def bfs
-    raise NotImplementedError
+    return bfs_helper(@root, [])
+  end
+
+  def bfs_helper(node, traversal)
+    return traversal if node.nil?
   end
 
   # Useful for printing
