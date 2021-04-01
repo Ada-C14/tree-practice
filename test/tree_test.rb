8468db38349f2c16b355986bb5a8da2cb8a0a99f
sandy vasquez <sandyvasquez@sandys-MacBook-Pro.local>
2021-03-31 20:46:26 -0700

    most tests passing

--- test/tree_test.rb
+++ test/tree_test.rb
@@ -38,8 +38,8 @@ describe Tree do
 
     it "will return the tree in order" do
 
-      expect(tree_with_nodes.inorder).must_equal [{:key=>1, :value=>"Mary"}, {:key=>3, :value=>"Paul"}, 
-                                       {:key=>5, :value=>"Peter"}, {:key=>10, :value=>"Karla"}, 
+      expect(tree_with_nodes.inorder).must_equal [{:key=>1, :value=>"Mary"}, {:key=>3, :value=>"Paul"},
+                                       {:key=>5, :value=>"Peter"}, {:key=>10, :value=>"Karla"},
                                        {:key=>15, :value=>"Ada"}, {:key=>25, :value=>"Kari"}]
     end
   end
@@ -51,8 +51,8 @@ describe Tree do
     end
 
     it "will return the tree in preorder" do
-      expect(tree_with_nodes.preorder).must_equal [{:key=>5, :value=>"Peter"}, {:key=>3, :value=>"Paul"}, 
-                                        {:key=>1, :value=>"Mary"}, {:key=>10, :value=>"Karla"}, 
+      expect(tree_with_nodes.preorder).must_equal [{:key=>5, :value=>"Peter"}, {:key=>3, :value=>"Paul"},
+                                        {:key=>1, :value=>"Mary"}, {:key=>10, :value=>"Karla"},
                                         {:key=>15, :value=>"Ada"}, {:key=>25, :value=>"Kari"}]
     end
   end
@@ -63,8 +63,8 @@ describe Tree do
     end
 
     it "will return the tree in postorder" do
-      expect(tree_with_nodes.postorder).must_equal [{:key=>1, :value=>"Mary"}, {:key=>3, :value=>"Paul"}, 
-                                         {:key=>25, :value=>"Kari"}, {:key=>15, :value=>"Ada"}, 
+      expect(tree_with_nodes.postorder).must_equal [{:key=>1, :value=>"Mary"}, {:key=>3, :value=>"Paul"},
+                                         {:key=>25, :value=>"Kari"}, {:key=>15, :value=>"Ada"},
                                          {:key=>10, :value=>"Karla"}, {:key=>5, :value=>"Peter"}]
     end
   end
@@ -75,8 +75,8 @@ describe Tree do
     end
 
     it "will return an array of a level-by-level output of the tree" do
-      expect(tree_with_nodes.bfs).must_equal [{:key=>5, :value=>"Peter"}, {:key=>3, :value=>"Paul"}, 
-                                   {:key=>10, :value=>"Karla"}, {:key=>1, :value=>"Mary"}, 
+      expect(tree_with_nodes.bfs).must_equal [{:key=>5, :value=>"Peter"}, {:key=>3, :value=>"Paul"},
+                                   {:key=>10, :value=>"Karla"}, {:key=>1, :value=>"Mary"},
                                    {:key=>15, :value=>"Ada"}, {:key=>25, :value=>"Kari"}]
     end
   end
