14ea98dc6bce322f6853d1ba2578bb160956a134
Kimberly Vitug <kim_vitug05@yahoo.com>
2021-03-31 19:59:06 -0700

    add solutions for add - postorder methods

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,12 +2,12 @@ class TreeNode
   attr_reader :key, :value
   attr_accessor :left, :right
 
-   def initialize(key, val)
+  def initialize(key, val)
     @key = key
     @value = val
     @left = nil
     @right = nil
-   end
+  end
 end
 
 class Tree
@@ -19,31 +19,107 @@ class Tree
   # Time Complexity: 
   # Space Complexity: 
   def add(key, value)
-    raise NotImplementedError
+    new_node = TreeNode.new(key, value)
+
+    @root = add_helper(root, new_node)
   end
+
+  def add_helper(current, new_node)
+    return new_node if current.nil?
+
+    if new_node.key <= current.key
+      current.left = add_helper(current.left, new_node)
+    else
+      current.right = add_helper(current.right, new_node)
+    end
+
+    return current
+  end 
 
   # Time Complexity: 
   # Space Complexity: 
   def find(key)
-    raise NotImplementedError
+    return nil if @root.nil?
+
+    current = @root
+    found = false
+
+    while !found
+      if key < current.key
+        current = current.left
+      elsif key > current.key
+        current = current.right 
+      else
+        return current.value
+        found = true
+      end
+    end
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def inorder
-    raise NotImplementedError
+    return inorder_helper(root)
+  end
+
+  def inorder_helper(node)
+    bst_inorder = []
+
+    if node.nil?
+      node = @root
+    end
+
+    return bst_inorder if node.nil?
+
+    bst_inorder += inorder_helper(node.left) if node.left # a
+    bst_inorder << { key: node.key, value: node.value }
+    bst_inorder += inorder_helper(node.right) if node.right
+
+    return bst_inorder
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def preorder
-    raise NotImplementedError
+    return preorder_helper(root)
+  end
+
+  def preorder_helper(node)
+    bst_preorder = []
+
+    if node.nil?
+      node = @root
+    end
+
+    return bst_preorder if node.nil?
+
+    bst_preorder << { key: node.key, value: node.value }
+    bst_preorder += preorder_helper(node.left) if node.left
+    bst_preorder += preorder_helper(node.right) if node.right
+
+    return bst_preorder
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def postorder
-    raise NotImplementedError
+    return postorder_helper(root)
+  end
+
+  def postorder_helper(node)
+    bst_postorder = []
+
+    if node.nil?
+      node = @root
+    end
+
+    return bst_postorder if node.nil?
+
+    bst_postorder += postorder_helper(node.left) if node.left
+    bst_postorder += postorder_helper(node.right) if node.right
+    bst_postorder << { key: node.key, value: node.value }
+
+    return bst_postorder
   end
 
   # Time Complexity: 
