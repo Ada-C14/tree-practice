37a3afbcdeefee1bcf32b649b8576fe4df626a7d
Iris Lux <iris.lux0@gmail.com>
2021-03-31 18:55:09 -0700

    completed all required exercises

--- lib/tree.rb
+++ lib/tree.rb
@@ -16,41 +16,75 @@ class Tree
     @root = nil
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
-  def add(key, value)
-    raise NotImplementedError
+  # Time Complexity: O(log(n))
+  # Space Complexity: O(log(n))
+  def add(key, value = nil, current = @root)
+    @root = TreeNode.new(key, value) and return if @root.nil?
+
+    if key <= current.key
+      current.left = TreeNode.new(key, value) and return if current.left.nil?
+      add(key, value, current.left)
+    else 
+      current.right = TreeNode.new(key, value) and return if current.right.nil?
+      add(key, value, current.right)
+    end
+
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
-  def find(key)
-    raise NotImplementedError
+  # Time Complexity: O(log(n))
+  # Space Complexity: O(log(n))
+  def find(key, current = @root)
+    return nil if current.nil? 
+
+    if key < current.key
+      find(key, current.left)
+    elsif key > current.key
+      find(key, current.right)
+    else
+      return current.value
+    end
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
-  def inorder
-    raise NotImplementedError
+  # Time Complexity: O(n)
+  # Space Complexity: O(n)
+  def inorder(current = @root, values = [])
+    return values if current.nil?
+    inorder(current.left, values)
+    values << {:key => current.key, :value => current.value}
+    inorder(current.right, values)
+
+    return values
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
-  def preorder
-    raise NotImplementedError
+  # Time Complexity: O(n)
+  # Space Complexity: O(n)
+  def preorder(current = @root, values = [])
+    return values if current.nil?
+    values << {:key => current.key, :value => current.value}
+    preorder(current.left, values)
+    preorder(current.right, values)
+    return values
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
-  def postorder
-    raise NotImplementedError
+  # Time Complexity: O(n)
+  # Space Complexity: O(n)
+  def postorder(current = @root, values = [])
+    return values if current.nil?
+    
+    postorder(current.left, values)
+    postorder(current.right, values)
+    values << {:key => current.key, :value => current.value}
+    return values
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
-  def height
-    raise NotImplementedError
+  # Time Complexity: O(n)
+  # Space Complexity: O(n)
+  def height(current = @root)
+    return 0 if current.nil? 
+
+    return 1 + [height(current.left), height(current.right)].max
   end
+
 
   # Optional Method
   # Time Complexity: 
