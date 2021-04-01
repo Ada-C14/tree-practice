7bcb3806cbcb61487144295858dc652979bbcc97
Yinjing Lin <lyingjin@amazon.com>
2021-03-31 23:05:54 -0700

    complete, except optional

--- lib/tree.rb
+++ lib/tree.rb
@@ -8,6 +8,10 @@ class TreeNode
     @left = nil
     @right = nil
    end
+
+   def to_pair
+     return {:key=>@key, :value=>@value}
+   end
 end
 
 class Tree
@@ -16,43 +20,45 @@ class Tree
     @root = nil
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
-  def add(key, value)
-    raise NotImplementedError
+  # Time Complexity: O(log(n))
+  # Space Complexity: O(log(n))
+  def add(key, value = nil)
+    @root = add_under_root(key, value, @root)
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  # Time Complexity: O(log(n))
+  # Space Complexity: O(log(n))
   def find(key)
-    raise NotImplementedError
+    return find_under_root(key, @root)
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  # Time Complexity: O(n)
+  # Space Complexity: O(n)
   def inorder
-    raise NotImplementedError
+    result = []
+    in_order_under_root(@root, result)
+    return result
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  # Time Complexity: O(n)
+  # Space Complexity: O(n)
   def preorder
-    raise NotImplementedError
+    return pre_order_under_root(@root)
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  # Time Complexity: O(n)
+  # Space Complexity: O(n)
   def postorder
-    raise NotImplementedError
+    return post_order_under_root(@root)
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  # Time Complexity: O(n)
+  # Space Complexity: O(log(n))
   def height
-    raise NotImplementedError
+    return height_under_root(@root)
   end
 
-  # Optional Method
+   # Optional Method
   # Time Complexity: 
   # Space Complexity: 
   def bfs
@@ -62,5 +68,87 @@ class Tree
   # Useful for printing
   def to_s
     return "#{self.inorder}"
+  end
+
+private
+
+  def add_under_root(key, value, root)
+    if root.nil?
+      root = TreeNode.new(key, value)
+    elsif key >= root.key
+      root.right = add_under_root(key, value, root.right)
+    else
+      root.left = add_under_root(key, value, root.left)
+    end
+    return root
+  end
+
+  def find_under_root(key, root)
+    if root.nil?
+      return nil
+    elsif root.key == key
+      return root.value 
+    elsif key >= root.key
+      find_under_root(key, root.right)
+    else
+      find_under_root(key, root.left)
+    end
+  end
+
+  def in_order_under_root(root, result)
+    if root.nil?
+      return nil
+    else
+      if root.left != nil
+        in_order_under_root(root.left, result)
+      end
+
+      result.push(root.to_pair)
+
+      if root.right != nil
+        in_order_under_root(root.right, result)
+      end
+    end
+    return root
+  end
+
+  def pre_order_under_root(root)
+    result = []
+    if !root.nil?
+      result.push(root.to_pair)
+      result = result.concat(pre_order_under_root(root.left))
+      result = result.concat(pre_order_under_root(root.right))
+    end
+    return result
+  end
+
+  def post_order_under_root(root)
+    result = []
+    if !root.nil?
+      result = result.concat(post_order_under_root(root.left))
+      result = result.concat(post_order_under_root(root.right))
+      result.append(root.to_pair)
+    end
+    return result
+  end
+
+  def height_under_root(root)
+    left_height = 0
+    right_height = 0
+
+    if root.nil?
+      return 0
+    else
+      left_height = height_under_root(root.left)
+      right_height = height_under_root(root.right)
+    end
+    return [left_height, right_height].max + 1
+
+
+    # if root.nil?
+    #   return 0
+    # else
+    #   return 1 + [height_under_root(root.left), height_under_root(root.right)].max
+    # end
   end
 end
