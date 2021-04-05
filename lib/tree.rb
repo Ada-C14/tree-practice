89d0fa1f32fcc7e6c9b5552bd189d0b502ced6b4
Beatrice T <be@trice.codes>
2021-04-04 20:38:02 -0700

    this is a commitment message

--- lib/tree.rb
+++ lib/tree.rb
@@ -16,22 +16,52 @@ class Tree
     @root = nil
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  # Time Complexity: O(log n)
+  # Space Complexity: O(n) - call stack
   def add(key, value)
-    raise NotImplementedError
+    if @root.nil?
+      @root = TreeNode.new(key, value)
+      return true
+    end
+
+    add_helper(key, value, @root)
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
-  def find(key)
-    raise NotImplementedError
+  def add_helper(key, value, current)
+    return TreeNode.new(key, value) if current.nil?
+
+    if key < current.key
+      current.left = add_helper(key, value, current.left)
+    else
+      current.right = add_helper(key, value, current.right)
+    end
   end
 
-  # Time Complexity: 
+  # Time Complexity: O(log n)
+  # Space Complexity: O(n) - call stack
+  def find(key, current = @root)
+    return nil if current.nil?
+    return current.value if current.key == key
+
+    if key < current.key
+      return find(key, current.left)
+    else
+      return find(key, current.right)
+    end
+  end
+
+  # Time Complexity:
   # Space Complexity: 
-  def inorder
-    raise NotImplementedError
+  def inorder(current = @root, answer = [])
+
+    # Current
+    unless current.nil?
+      answer += inorder(current.left, answer)
+      return {key: current.key, value: current.value}
+      answer += inorder(current.right, answer)
+    end8
+
+    return answer
   end
 
   # Time Complexity: 
@@ -64,3 +94,11 @@ class Tree
     return "#{self.inorder}"
   end
 end
+
+# tree = Tree.new
+# tree.add(5, "Peter")
+# tree.add(3, "Paul")
+# tree.add(1, "Mary")
+# tree.add(10, "Karla")
+# tree.add(15, "Ada")
+# tree.add(25, "Kari")
