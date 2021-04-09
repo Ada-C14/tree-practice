9790aecdbf16c56e83d7f02626d149f4cebd3f67
dnsrocha <drocha@gmail.com>
2021-04-08 17:00:50 -0700

    implemented height

--- lib/tree.rb
+++ lib/tree.rb
@@ -16,10 +16,38 @@ class Tree
     @root = nil
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
-  def add(key, value)
-    raise NotImplementedError
+  # Time Complexity: log(n)
+  # Space Complexity: log(n)
+
+  def add_helper(key, value, current)
+
+    if current.key == key
+      current.value = value
+    elsif current.key > key
+      if current.left == nil
+        current.left = TreeNode.new(key,value)
+      else
+        add_helper(key, value, current.left)
+      end
+    else
+      if current.right == nil
+        current.right = TreeNode.new(key, value)
+      else
+        add_helper(key, value, current.right)
+      end
+    end
+
+  end
+
+  def add(key, value = nil)
+    #what should I do to associate the value to the key when building the binaty tree search?
+    if @root.nil?
+      @root = TreeNode.new(key,value) #?? // what about the value?
+      return @root
+    else
+      add_helper(key, value, @root)
+    end
+
   end
 
   # Time Complexity: 
@@ -30,8 +58,16 @@ class Tree
 
   # Time Complexity: 
   # Space Complexity: 
-  def inorder
-    raise NotImplementedError
+
+  def inorder(current = @root, values = [])  
+    if current.nil?
+      return values
+    end
+
+    inorder(current.left, values)
+    values.push(current.value)
+    inorder(current.right, values)
+    return values
   end
 
   # Time Complexity: 
@@ -46,10 +82,20 @@ class Tree
     raise NotImplementedError
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
-  def height
-    raise NotImplementedError
+  # Time Complexity: o(n)
+  # Space Complexity: O(h), where h = height
+  def height(current = @root)
+    return 0 if current.nil?
+    
+    left_child = height(current.left)
+    right_child = height(current.right)
+
+    if left_child > right_child
+      return left_child + 1
+    else
+      return right_child + 1
+    end
+
   end
 
   # Optional Method
