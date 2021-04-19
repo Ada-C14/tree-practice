7c4849b5b84f46276ab3db733e312b719763e849
Anya Tokar <anya.tokar@moz.com>
2021-04-18 22:06:02 -0700

    removed unnecessary helpers

--- lib/tree.rb
+++ lib/tree.rb
@@ -16,89 +16,61 @@ class Tree
     @root = nil
   end
 
-  # Time Complexity: O(log n)
-  # Space Complexity: O(1)
-  def add_helper(key, value, current)
+  # Time Complexity: O(log n) for a balanced tree, O(n) for an unbalanced tree
+  # Space Complexity: same as time complexity
+  def add(key, value = nil, current = @root)
+    return @root = TreeNode.new(key, value) if current.nil?
     if key <= current.key
-      current.left ? add_helper(key, value, current.left) : current.left = TreeNode.new(key, value)
+      current.left ? add(key, value, current.left) : current.left = TreeNode.new(key, value)
     else
-      current.right ? add_helper(key, value, current.right) : current.right = TreeNode.new(key, value)
+      current.right ? add(key, value, current.right) : current.right = TreeNode.new(key, value)
     end
   end
 
-  def add(key, value = nil)
-    if @root.nil?
-      @root = TreeNode.new(key, value)
-      return @root
-    end
-    add_helper(key, value, @root)
-  end
-
-  # Time Complexity: O(log n)
-  # Space Complexity: O(1)
-  def find_helper(key, current)
+  # Time Complexity: O(log n) for a balanced tree, O(n) for an unbalanced tree
+  # Space Complexity: same as time complexity
+  def find(key, current = @root)
     return nil if current.nil?
     return current.value if current.key == key
     if key < current.key
-      find_helper(key, current.left)
+      find(key, current.left)
     else key > current.key
-      find_helper(key, current.right)
+      find(key, current.right)
     end
   end
 
-  def find(key)
-    find_helper(key, @root)
+  # Time Complexity: 0(n) with n being number of nodes in the tree
+  # Space Complexity: O(n)
+  def inorder(current = @root, elements = [])
+    return elements if current.nil?
+    inorder(current.left, elements)
+    elements << {:key => current.key, :value => current.value}
+    inorder(current.right, elements)
   end
 
   # Time Complexity: 0(n) with n being number of nodes in the tree
   # Space Complexity: O(n)
-  def inorder_helper(current, elements)
+  def preorder(current = @root, elements = [])
     return elements if current.nil?
-    inorder_helper(current.left, elements)
     elements << {:key => current.key, :value => current.value}
-    inorder_helper(current.right, elements)
-  end
-
-  def inorder
-    inorder_helper(current = @root, elements = [])
+    preorder(current.left, elements)
+    preorder(current.right, elements)
   end
 
   # Time Complexity: 0(n) with n being number of nodes in the tree
   # Space Complexity: O(n)
-  def preorder_helper(current, elements)
+  def postorder(current = @root, elements = [])
     return elements if current.nil?
+    postorder(current.left, elements)
+    postorder(current.right, elements)
     elements << {:key => current.key, :value => current.value}
-    preorder_helper(current.left, elements)
-    preorder_helper(current.right, elements)
-  end
-
-
-  def preorder
-    preorder_helper(current = @root, elements = [])
-  end
-
-  # Time Complexity: 0(n) with n being number of nodes in the tree
-  # Space Complexity: O(n)
-  def postorder_helper(current, elements)
-    return elements if current.nil?
-    postorder_helper(current.left, elements)
-    postorder_helper(current.right, elements)
-    elements << {:key => current.key, :value => current.value}
-  end
-
-  def postorder
-    postorder_helper(current = @root, elements = [])
   end
 
   # Time Complexity: O(n)
   # Space Complexity: O(n)
-  def height_helper(current)
+  def height(current = @root)
     return 0 if current.nil?
-    return 1 + [height_helper(current.left), height_helper(current.right)].max 
-  end
-  
-  def height
-    height_helper(@root)
+    return 1 + [height(current.left), height(current.right)].max 
   end
 
   # Optional Method
