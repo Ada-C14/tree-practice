9a97081462dee65b6f6c05152a58a060c66ef07d
Rachael Gomez <rachael.gomez@nordstrom.com>
2021-06-02 15:44:34 -0700

    add and find done

--- lib/tree.rb
+++ lib/tree.rb
@@ -19,13 +19,52 @@ class Tree
   # Time Complexity: 
   # Space Complexity: 
   def add(key, value)
-    raise NotImplementedError
+    # there may or may not be a root
+    # if there's no root then the key/value will be the new root
+    # otherwise check to see if value is bigger or smaller than the root value
+    # initialize new node with the key and value
+    # check if it's smaller or larger and if it's the last node of the branch
+    # if it's the last node then add it after that node
+    input = TreeNode.new(key,value)
+    if @root == nil 
+      @root = input
+      return
+    else here = @root
+    end
+    while here != nil
+      if input.key < here.key
+        if here.left != nil
+          here = here.left
+        else here.left = input
+          return
+        end
+      else input.key > here.key
+        if here.right != nil
+          here = here.right
+        else here.right = input
+          return
+        end
+      end
+    end  
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def find(key)
-    raise NotImplementedError
+    if @root == nil
+      return nil
+    end
+    here = @root
+    while here != nil
+      if here.key == key
+        return here.value
+      elsif key < here.key
+          here = here.left
+      elsif key > here.key
+          here = here.right
+      end
+    end
+    return nil
   end
 
   # Time Complexity: 
